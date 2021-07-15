7d925c84e319fb0f417ee3f2ccda5cf34ca24ad4
endeu <wendellpguariento@gmail.com>
2021-07-15 20:35:05 -0300

    Class 'pessoa' adicionado

---
+++ Bootcamp Santander Kotlin/src/main/kotlin/one/digitalinnovation/digitalbank/pessoa.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digitalbank
+
+class pessoa {
+    //atributos de uma pessoa
+    //variaveis da classe pessoa
+    var nome: String = "Wendell"
+    var cpf: String = "123.123.123-13"
+}
+
+fun main() {
+    //ao criar a variável "Wendell" e instanciar à classe pessoa()
+    //define que o estado e comportamento de "Wendell", que no caso é uma pessoa
+    val Wendell = pessoa()
+
+    //saídas
+    println(Wendell.nome)
+    println(Wendell.cpf)
+}
