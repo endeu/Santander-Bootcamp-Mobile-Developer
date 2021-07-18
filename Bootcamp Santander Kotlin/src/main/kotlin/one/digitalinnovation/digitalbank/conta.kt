17f36baa643f76f10eaef8c1ecc6cbb4161a4ce4
endeu <wendellpguariento@gmail.com>
2021-07-18 14:05:52 -0300

    classe 'conta' criada.

---
+++ Bootcamp Santander Kotlin/src/main/kotlin/one/digitalinnovation/digitalbank/conta.kt
@@ -0,0 +1,20 @@
+package one.digitalinnovation.digitalbank
+
+import java.math.BigDecimal
+
+class conta (
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+    ){
+
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+
+
+}
