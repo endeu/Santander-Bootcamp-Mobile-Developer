936cefadacf24c34f39dccecbfff043c6841ceb1
endeu <wendellpguariento@gmail.com>
2021-07-19 21:54:52 -0300

    enum e teste para enum criado

---
+++ Bootcamp Santander Kotlin/src/main/kotlin/one/digitalinnovation/digitalbank/testes/TesteClienteTipoEnum.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digitalbank.testes
+
+import one.digitalinnovation.digitalbank.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.descricao}")
+
+}
