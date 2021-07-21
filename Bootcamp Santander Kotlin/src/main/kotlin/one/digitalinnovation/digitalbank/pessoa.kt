84bb77f34ca446aeaca91b1dd821320f76402bec
endeu <wendellpguariento@gmail.com>
2021-07-21 19:24:45 -0300

    'TestePessoa' excluído e abstração 'pessoa' criado

--- Bootcamp Santander Kotlin/src/main/kotlin/one/digitalinnovation/digitalbank/pessoa.kt
+++ Bootcamp Santander Kotlin/src/main/kotlin/one/digitalinnovation/digitalbank/pessoa.kt
@@ -1,8 +1,8 @@
 package one.digitalinnovation.digitalbank
 
-open class pessoa(
+abstract class pessoa(
     //atributos de uma pessoa
     //variaveis da classe pessoa
-    open val nome: String = "Wendell",
-    open val cpf: String = "123.123.123-13"
+    val nome: String = "Wendell",
+    val cpf: String = "123.123.123-13"
 ){}
